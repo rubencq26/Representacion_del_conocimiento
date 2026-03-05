788f1e88c6682678d8eded00f9a334c0ee689ba1
Ruben Conde <144829066+rubencq26@users.noreply.github.com>
2026-03-05 08:45:14 +0100

    Add resistor color codes and color list
    Define color codes for resistor values and a list of colors.

---
+++ EjemplosProlog/resistencia.pl
@@ -0,0 +1,14 @@
+color_code("black", 0).
+color_code("brown", 1).
+color_code("red", 2).
+color_code("orange",3).
+color_code("yellow", 4).
+color_code("green", 5).
+color_code("blue", 6).
+color_code("violet", 7).
+color_code("grey", 8).
+color_code("white", 9).
+
+
+colors(["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]).
+
